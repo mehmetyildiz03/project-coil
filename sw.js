51331f9f4affc6e1e5e4d04ee011db6334337243
Mehmet Yıldız <115743794+mehmetyildiz03@users.noreply.github.com>
2026-09-16 00:01:52 +0300

    Cache complete PWA app shell for offline play

--- sw.js
+++ sw.js
@@ -1,11 +1,14 @@
-const CACHE = 'project-coil-pwa-v1';
+const CACHE = 'project-coil-pwa-v2';
 const APP_SHELL = [
   './',
   './index.html',
   './style.css',
   './game.js',
   './manifest.webmanifest',
-  './icon.svg'
+  './icon.svg',
+  './icons/app-icon-192.png',
+  './icons/app-icon-512.png',
+  './icons/apple-touch-icon.png'
 ];
 
 self.addEventListener('install', event => {
